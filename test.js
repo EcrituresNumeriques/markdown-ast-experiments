16f7c6a1c51c5a914ccbb9ce78ab075970035a1e
Guillaume Grossetie <ggrossetie@gmail.com>
2020-12-16 10:37:45 +0100

    Use a simpler file

--- test.js
+++ test.js
@@ -3,13 +3,31 @@ const util = require('util')
 const unified = require('unified')
 const markdown = require('remark-parse')
 
-const tree = unified().use(markdown).parse(fs.readFileSync('input.md', 'utf8'))
+const markdownFile = 'short.md'
+const tree = unified().use(markdown).parse(fs.readFileSync(markdownFile, 'utf8'))
+
+function prunePosition(obj) {
+  for (let property in obj) {
+    if (obj.hasOwnProperty(property)) {
+      if (typeof obj[property] == "object") {
+        if (property === 'position') {
+          delete obj[property]
+        } else {
+          prunePosition(obj[property])
+        }
+      }
+    }
+  }
+}
+prunePosition(tree)
+
 fs.writeFileSync('mdast.json', JSON.stringify(tree, null, '  '), 'utf8')
 fs.writeFileSync('pandoc-ast.json', JSON.stringify(JSON.parse(fs.readFileSync('pandoc-ast.json', 'utf8')), null, '  '), 'utf8')
 
-
+/*
 console.log(util.inspect(tree, {showHidden: false, depth: null}))
 console.log('')
 console.log('')
 console.log('')
 console.log(util.inspect(JSON.parse(fs.readFileSync('pandoc-ast.json', 'utf8')), {showHidden: false, depth: null}))
+*/
